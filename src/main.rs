1d77dff81325150ab3e347d2c7509ec384a7f210
Stian Soltvedt <soltvedt.stian@gmail.com>
2018-03-26 16:08:44 +0200

    Move filename argument to a command line option

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+extern crate clap;
 extern crate image;
 extern crate pathfinding;
 
+use clap::{Arg, App};
 use image::{ImageBuffer, Rgb};
 use pathfinding::astar::astar;
 
@@ -26,7 +28,18 @@ impl Pos {
 }
 
 fn main() {
-    let filepath = "c:\\users\\stian\\downloads\\maze.jpg";
+    let matches = App::new("Maze Solver")
+        .version("1.0")
+        .author("Stian S. <soltvedt.stian@gmail.com>")
+        .about("Solves a maze provided with an image file.")
+        .arg(Arg::with_name("INPUT")
+            .help("Sets the input file to use")
+            .required(true)
+            .index(1))
+        .get_matches();
+    
+    let filepath = matches.value_of("INPUT").unwrap();
+
     let mut rgb = image::open(filepath).expect("Failed to open image")
         .grayscale().adjust_contrast(std::f32::MAX)
         .to_rgb();
@@ -46,8 +59,8 @@ fn main() {
 
         println!("Path length: {}", path.len());
 
-        rgb.save("c:\\users\\stian\\downloads\\maze_solved.png")
-        .expect("Failed to save image");
+        rgb.save(format!("{}{}", filepath, "_solved.png"))
+            .expect("Failed to save image");
     } else {
         println!("No path found");
     }
