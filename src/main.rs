1e7a8b2e304e0589d33dfee92db067ff72719527
Stian Soltvedt <soltvedt.stian@gmail.com>
2018-03-27 09:47:46 +0200

    Run rustfmt

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ extern crate clap;
 extern crate image;
 extern crate pathfinding;
 
-use clap::{Arg, App};
+use clap::{App, Arg};
 use image::{ImageBuffer, Rgb};
 use pathfinding::astar::astar;
 
@@ -10,21 +10,21 @@ use pathfinding::astar::astar;
 struct Pos(u32, u32);
 
 impl Pos {
-  fn distance(&self, other: &Pos) -> usize {
-    ((self.0 - other.0) + (self.1 - other.1)) as usize
-  }
+    fn distance(&self, other: &Pos) -> usize {
+        ((self.0 - other.0) + (self.1 - other.1)) as usize
+    }
 
-  fn neighbours(&self, img: &ImageBuffer<Rgb<u8>, Vec<u8>>) -> Vec<(Pos, usize)> {
-    let &Pos(x, y) = self;
-    let (w, h) = img.dimensions();
-    vec![Pos(x+1,y), Pos(x-1,y), Pos(x,y+1), Pos(x,y-1)]
+    fn neighbours(&self, img: &ImageBuffer<Rgb<u8>, Vec<u8>>) -> Vec<(Pos, usize)> {
+        let &Pos(x, y) = self;
+        let (w, h) = img.dimensions();
+        vec![Pos(x+1,y), Pos(x-1,y), Pos(x,y+1), Pos(x,y-1)]
         .into_iter()
         // Assuming underflow causes numbers larger than h & w
         .filter(|p| p.0 < w && p.1 < h)
         .filter(|p| img.get_pixel(p.0, p.1).data[0] > 122)
         .map(|p| (p, 1))
         .collect()
-  }
+    }
 }
 
 fn main() {
@@ -32,16 +32,20 @@ fn main() {
         .version("1.0")
         .author("Stian S. <soltvedt.stian@gmail.com>")
         .about("Solves a maze provided by an image file.")
-        .arg(Arg::with_name("INPUT")
-            .help("Sets the input file to use")
-            .required(true)
-            .index(1))
+        .arg(
+            Arg::with_name("INPUT")
+                .help("Sets the input file to use")
+                .required(true)
+                .index(1),
+        )
         .get_matches();
-    
+
     let filepath = matches.value_of("INPUT").unwrap();
 
-    let mut rgb = image::open(filepath).expect("Failed to open image")
-        .grayscale().adjust_contrast(std::f32::MAX)
+    let mut rgb = image::open(filepath)
+        .expect("Failed to open image")
+        .grayscale()
+        .adjust_contrast(std::f32::MAX)
         .to_rgb();
 
     static GOAL: Pos = Pos(0, 2047);
@@ -49,12 +53,12 @@ fn main() {
         &Pos(0, 1990),
         |p| p.neighbours(&rgb),
         |p| p.distance(&GOAL) / 3,
-        |p| *p == GOAL
+        |p| *p == GOAL,
     );
 
     if let Some((path, _)) = result {
         for p in &path {
-            rgb.put_pixel(p.0, p.1, Rgb {data: [255, 0, 0]});
+            rgb.put_pixel(p.0, p.1, Rgb { data: [255, 0, 0] });
         }
 
         println!("Path length: {}", path.len());
